51fd8c0cdb1fcbebd38c4701a733ba77cbadad72
coderj001 <amirajubolchi001@gmail.com>
2021-08-06 19:34:02 +0530

    added whitenoise

--- PizzaAPI/settings.py
+++ PizzaAPI/settings.py
@@ -30,7 +30,12 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = list()
+
+if not DEBUG:
+    MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware']
+
+MIDDLEWARE += [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +130,6 @@ SWAGGER_SETTINGS = {
 REDOC_SETTINGS = {
     'LAZY_RENDERING': False,
 }
+
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
